392bc0cb96e8ec836b4e31cb0eb244b376c23fdb
cferreirasuazo@gmail.com <cferreirasuzo@gmail.com>
2025-02-12 17:43:03 -0400

    Configure schadcn/ui

--- frontend_lang_portal/src/App.tsx
+++ frontend_lang_portal/src/App.tsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import { Button } from "./components/ui/button";
 export default function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
         Hello, Tailwind with Vite! 🚀
       </h1>
+      <Button>Hit me harder</Button>
     </div>
   );
 }
